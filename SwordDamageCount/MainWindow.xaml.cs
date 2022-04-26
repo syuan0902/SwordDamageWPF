310e3164050649caff5466bd23159beac1351d38
syuan7492 <kurosaki7492@gmail.com>
2022-04-27 03:30:25 +0800

    Add project files.

---
+++ SwordDamageCount/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SwordDamageCount
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+        SwordDamage swordDamage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            swordDamage = new SwordDamage(random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7));
+
+            DisplayDamage();
+        }
+        
+        public void RollDice()
+        {
+            swordDamage.Roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            DisplayDamage();
+        }
+
+        private void DisplayDamage()
+        {
+            totalDamage.Text = $"擲出 {swordDamage.Roll}點, 造成 {swordDamage.Damage}HP傷害";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RollDice();
+        }
+
+        private void flameSword_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Flaming = true;
+            DisplayDamage();
+        }
+
+        private void flameSword_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Flaming = false;
+            DisplayDamage();
+        }
+
+        private void magic_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Magic = true;
+            DisplayDamage();
+        }
+
+        private void magic_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Magic = false;
+            DisplayDamage();
+        }
+    }
+}
---
+++ SwordDamageCount/SwordDamage.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SwordDamageCount
+{
+    class SwordDamage
+    {
+
+        private const int BASE_DAMAGE = 3;
+        private const int FLAME_DAMAGE = 2;
+
+        //支援欄位
+        private int roll;
+        private bool magic;
+        private bool flaming;
+
+        public int Damage { get; private set; }
+        
+        /// <summary>
+        /// 建構式會用預設的Magic與Flaming
+        /// 並使用3d6擲法來計算傷害
+        /// </summary>
+        /// <param name="startingRoll">開始擲3d6</param>
+        public SwordDamage(int startingRoll)
+        {
+            roll = startingRoll;
+            CalculateDamage();
+        }
+
+        /// <summary>
+        /// 設定或取得3d6擲法
+        /// </summary>
+        public int Roll 
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+
+        }
+
+        /// <summary>
+        /// 如果是魔法劍，則是true，否則是false
+        /// </summary>
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                if (value)
+                {
+                    magic = value;
+                    CalculateDamage();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 如果是火焰劍，則是true，否則是false
+        /// </summary>
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CalculateDamage()
+        {
+            decimal m_magicMultiplier = 1m;
+            if (Magic) m_magicMultiplier = 1.75m;
+
+            Damage = BASE_DAMAGE;
+
+            Damage = (int)(Roll * m_magicMultiplier) + BASE_DAMAGE;
+
+            if (Flaming) Damage += FLAME_DAMAGE;
+        }
+    }
+}
